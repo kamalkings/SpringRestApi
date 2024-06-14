f16ea8b982afb4485ed2d7905f8c10f0af0e9a7c
viratkohli-18 <kamalece@hotmail.com>
2024-06-15 03:13:38 +0530

    forked branch
    vk18

--- src/main/java/xample/test/FirstBranchMain.java
+++ src/main/java/xample/test/FirstBranchMain.java
@@ -6,5 +6,6 @@ import spring.restapi.SpringRestApi;
 public class FirstBranchMain {
     public static void main(String[] args) {
         System.out.println("FirstBranch");
+          System.out.println("This is from forked branch of vk18 from original collab of kamalkings");
     }
 }
