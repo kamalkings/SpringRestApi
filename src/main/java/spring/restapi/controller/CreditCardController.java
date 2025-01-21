f6c8c41647d83dd76216825fbfeef9afde945427
dkamalraj <yakamalkings@gmail.com>
2025-01-22 01:46:00 +0530

    RestAPI Delete Mapping

--- src/main/java/spring/restapi/controller/CreditCardController.java
+++ src/main/java/spring/restapi/controller/CreditCardController.java
@@ -1,6 +1,7 @@
 package spring.restapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,10 @@ public class CreditCardController {
     @GetMapping("/getCreditCards/{bankName}")
     public CreditCard getByBank(@PathVariable String bankName){
         return creditCardService.getByBank(bankName);
+    }
+
+    @DeleteMapping ("/removeCreditCards/{bankName}")
+    public void removeByBank(@PathVariable String bankName){
+         creditCardService.removeby(bankName);
     }
 }
--- src/main/java/spring/restapi/service/CreditCardService.java
+++ src/main/java/spring/restapi/service/CreditCardService.java
@@ -18,7 +18,7 @@ static{
   creditCardList.add(new CreditCard("HDFC",8507,LocalDate.of(2026,9,01)));
   creditCardList.add(new CreditCard("SBI",1089,LocalDate.of(2027,9,01)));
   creditCardList.add(new CreditCard("KTK",7646,LocalDate.of(2028,9,01)));
-  creditCardList.add(new CreditCard("KTK",1234,LocalDate.of(2028,9,01)));
+  creditCardList.add(new CreditCard("FED",1234,LocalDate.of(2028,9,01)));
 }
 
 public List<CreditCard>getallcards(){
@@ -29,5 +29,10 @@ public List<CreditCard>getallcards(){
   public CreditCard getByBank(String name) {
     Predicate<? super CreditCard> predicate = creditcard -> creditcard.getName().equals(name);
     return creditCardList.stream().filter(predicate).findFirst().orElse(null);
+  }
+
+  public void removeby(String bankName) {
+    Predicate<? super CreditCard> predicate = creditcard -> creditcard.getName().equals(bankName);
+    creditCardList.removeIf(predicate);
   }
 }
